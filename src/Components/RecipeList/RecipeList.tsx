7f700a1b93b197a940da1c1546df0adc08748dc6
bestdevelope <leabouillot7@gmail.com>
2024-07-16 10:28:27 +0200

    basse de données db json

--- src/Components/RecipeList/RecipeList.tsx
+++ src/Components/RecipeList/RecipeList.tsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
 const RecipeList = () => {
-  return (
-    <div>RecipeList</div>
-  )
-}
+  const [recipes, setRecipes] = useState([]);
 
-export default RecipeList
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const response = await fetch(
+      "https://api.spoonacular.com/recipes/random?apikey=${process.env.RECIPE_API_KEY}"
+    );
+    const data = await response.json();
+    setRecipes(data.recipes);
+  };
+
+  return (
+    <div>
+     {recipes.map((recipe) => {
+       return (
+         <Link to={`/recipe/${recipe.id}`}>
+           <div key={recipe.id}>
+             <h2>{recipe.title}</h2>
+             <img src={recipe.image} alt={recipe.title} />
+           </div>
+         </Link>
+       );
+     })}
+    </div>
+  );
+};
+
+export default RecipeList;
