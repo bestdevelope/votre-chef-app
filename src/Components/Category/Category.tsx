b55e2bf647d24c421cc4d1d93ee19d0aad515784
bestdevelope <leabouillot7@gmail.com>
2024-07-18 15:01:44 +0200

    correction

--- src/Components/Category/Category.tsx
+++ src/Components/Category/Category.tsx
@@ -1,29 +1,59 @@
-// import React from "react";
-// import { Link } from "react-router-dom";
+import { Link } from "react-router-dom";
+import "./Category.css";
 
-// function Category() {
-//   return (
-//     <List>
-//       <Link to={"./searchrecipe/entree"}>
-//         <p>Entrées</p>
-//       </Link>
-//       <Link>
-//         <p>Plats</p>
-//       </Link>
-//       <Link>
-//         <p>Desserts</p>
-//       </Link>
-//       <Link>
-//         <p>Italien</p>
-//       </Link>
-//       <Link>
-//         <p>Française</p>
-//       </Link>
-//       <Link>
-//         <p>Asiatique</p>
-//       </Link>
-//     </List>
-//   );
-// }
+interface Category {
+  id: number;
+  name: string;
+  category: string;
+}
 
-// export default Category;
+function Category() {
+
+
+  useEffect(() => {
+    const fetchRecipes = async () => {
+      try {
+        const data = await fetch("http://localhost:3000/recipes/category");
+        const recipes = await data.json();
+        setCategory(recipes.category);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    (async () => await fetchRecipes())();
+  }, [params.type]);
+
+  return (
+    <div className="list-category">
+      <div>
+        <span>{category}</span>
+      </div>
+      <div>
+        <ul className="list-category">
+          {[
+            "All",
+            "Entrées",
+            "Plats",
+            "Desserts",
+            "Français",
+            "Italien",
+            "Asiatique",
+          ].map((category) => {
+            return (
+              <li key={category}>
+                <Link
+                  className="link-category"
+                  to={`./recipes/${category.toLowerCase()}`}
+                >
+                  {category}
+                </Link>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default Category;
