695b63b07f1cf0266f07d436c00dfb7e813a3b14
bestdevelope <leabouillot7@gmail.com>
2024-07-19 20:12:19 +0200

    detail fetch

--- src/Components/RecipeDetail/RecipeDetail.tsx
+++ src/Components/RecipeDetail/RecipeDetail.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useCallback } from "react";
 import { useParams } from "react-router-dom";
 import Header from "../Header/Header";
 // import FavoriteButton from ""
@@ -19,29 +19,49 @@ interface Details {
 
 const RecipeDetail = () => {
   //
-  const params = useParams();
-  const [details, setDetails] = useState<Details | any>([]);
+  const { id } = useParams();
+  const [details, setDetails] = useState<Details[]>([]);
+
+  const fetchData = useCallback(async () => {
+    try {
+      const response = await fetch("http://localhost:3000/recipes/");
+      const data = await response.json();
+      const filterData = data.filter((item: Details) => item.id == id);
+      setDetails(filterData);
+    } catch (error) {
+      console.log(error);
+    }
+  }, [id]);
 
   useEffect(() => {
-    async function fetchData() {
-      try {
-        const response = await fetch(
-          "http://localhost:3000/recipes/" + params.id
-        );
-        const datas = await response.json();
-        if (datas) {
-          setDetails(datas);
-        }
-      } catch (err) {
-        setDetails([]);
-      }
-    }
     fetchData();
-  }, [params.id]);
+  }, [fetchData]);
+
+  console.log(details[0]);
 
   return (
     <div>
       <Header />
+      {/* <h1>{details.title}</h1>
+      <img src={details.image} alt={details.title} />
+      <p>Catégorie: {details.category}</p>
+      <p>Difficulté: {details.difficulty}</p>
+      <p>Prix: {details.price}</p>
+      <p>Temps de préparation: {details.time} minutes</p>
+      <p>Nombre de servings: {details.number_servings}</p>
+      <p>Description: {details.description}</p> */}
+      {/* <h2>Ingredients:</h2>
+      <ul>
+        {details.ingredients.map((index) => (
+          <li key={index}>{details.ingredients}</li>
+        ))}
+      </ul>
+      <h2>Instructions:</h2>
+      <ol>
+        {details.instructions.map((index) => (
+          <li key={index}>{details.instruction}</li>
+        ))}
+      </ol> */}
     </div>
   );
 };
