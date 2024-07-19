11bf6f824f2523a3a44fc7b3e360876a48183032
bestdevelope <leabouillot7@gmail.com>
2024-07-20 00:35:33 +0200

    favorite

--- src/Components/RecipeDetail/RecipeDetail.tsx
+++ src/Components/RecipeDetail/RecipeDetail.tsx
@@ -1,5 +1,6 @@
 import { useState, useEffect, useCallback } from "react";
 import { useParams } from "react-router-dom";
+import FavoriteRecipe from "../FavoriteRecipe/FavoriteRecipe";
 
 // import FavoriteButton from ""
 
@@ -61,7 +62,7 @@ const RecipeDetail = () => {
           <li key={index}>{details.instruction}</li>
         ))}
       </ol> */}
-
+      <FavoriteRecipe />
     </div>
   );
 };
