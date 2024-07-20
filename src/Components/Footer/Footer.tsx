ffd97c110d86cde102eab2786ed89236fe361de9
bestdevelope <leabouillot7@gmail.com>
2024-07-20 17:38:05 +0200

    footer

--- src/Components/Footer/Footer.tsx
+++ src/Components/Footer/Footer.tsx
@@ -1,13 +1,41 @@
-import React from "react";
+import "./Footer.css";
 
 function Footer() {
   return (
-    <div>
-      <p>�� 2022 Meilleurs Recettes. Tous droits réservés.</p>
-      <p>Développé par Chef-Codeur</p>
-      <p>Version 1.0.0</p>
-      <p>Contactez-nous: 0123456789</p>
-    </div>
+    <>
+      <footer id="footer">
+        <div className="container">
+          <h3>Votre Chef App</h3>
+          <p>
+            Découvrez les meilleures recettes de cuisine adaptées à vos goûts.
+          </p>
+          <div className="social-links">
+            <a href="#" className="twitter">
+              <i className="bx bxl-twitter"></i>
+            </a>
+            <a href="#" className="facebook">
+              <i className="bx bxl-facebook"></i>
+            </a>
+            <a href="#" className="instagram">
+              <i className="bx bxl-instagram"></i>
+            </a>
+            <a href="#" className="google-plus">
+              <i className="bx bxl-skype"></i>
+            </a>
+            <a href="#" className="linkedin">
+              <i className="bx bxl-linkedin"></i>
+            </a>
+          </div>
+          <div className="copyright">
+            &copy; Copyright{" "}
+            <strong>
+              <span>Léa</span>
+            </strong>
+            . All Rights Reserved
+          </div>
+        </div>
+      </footer>
+    </>
   );
 }
 
