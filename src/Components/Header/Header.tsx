786611167e2bb6632c3375c6567572f0c5d477ba
bestdevelope <leabouillot7@gmail.com>
2024-07-22 00:51:39 +0200

    link

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./Header.css";
 import logo from "../assets/logo-chef.png";
 import { Link } from "react-router-dom";
-import Category from "../Category/Category";
+// import Category from "../Category/Category";
 // import user_icon from "../assets/user.png";
 
 const Header: React.FC = () => {
@@ -12,15 +12,23 @@ const Header: React.FC = () => {
         <img src={logo} alt="logo" width={100} />
         <p>Meilleurs Recettes</p>
       </div>
-
-      <Category
-        // categories={categories}
-        // category={category}
-        // setCategory={setCategory}
-      />
+      <nav>
+        <ul>
+          <li>
+            <Link to="/" className="nav-list">
+              Recettes
+            </Link>
+          </li>
+          <li>
+            <Link to="/favorite/" className="nav-list">
+              Favoris
+            </Link>
+          </li>
+        </ul>
+      </nav>
 
       <div className="nav-login">
-        <Link to="/login">
+        <Link to="/login" className="nav-list">
           <button>Connexion</button>
         </Link>
       </div>
